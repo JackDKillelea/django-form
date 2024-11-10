dc0b03c2a461e700d12b9b830a93a18af7fc7d11
Jack Killelea <42438278+JackDKillelea@users.noreply.github.com>
2024-11-10 22:43:35 +0000

    Set up Django
    - Now set up and can run python manage.py runserver to view the website
    
    - Made the website look like the other flask app, and working. User can press submit. It will reset the page as expected without any errors.

--- job_application/models.py
+++ job_application/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Form(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.EmailField()
+    available_date = models.DateField()
+    current_status = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
